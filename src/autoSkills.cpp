09b2a47115fac006c3a392e075ff6f38a4527046
Miniongolf <miniongolf8@gmail.com>
2025-03-01 22:46:03 -0500

    Finish skills (ggs)

--- src/autoSkills.cpp
+++ src/autoSkills.cpp
@@ -18,8 +18,8 @@ void auton::skills() {
     startTime = from_msec(pros::millis());
     // Set colour sort to red alliance
     intake.setAlliance(Alliance::RED);
-
     // Start pose (in front of alliance stake)
+    /*
     chassis.setPose(-64, 0, 90);
     intake.forwards();
     pros::delay(700);
@@ -49,7 +49,7 @@ void auton::skills() {
     chassis.swingToPoint(-48, 60, lemlib::DriveSide::RIGHT, 1000, {}, false);
     chassis.safeMoveToPoint(-48, 60, 1000, {}, false);
     intake.setMode(Intake::modes::INDEX);
-    
+
     // Drop first goal in corner
     chassis.turnToPoint(-70, 70, 600, {.forwards = false});
     chassis.moveTimed(-50, 0, 600, false);
@@ -68,8 +68,9 @@ void auton::skills() {
     pros::delay(600);
     robot::scoreWallStake(true, true);
     robot::printPose();
+    */
 
-    // chassis.setPose(0, 61, 0); // Odom reset on top wallstake, don't preserve imu heading
+    chassis.setPose(5, 61, 0); // Odom reset on top wallstake, don't preserve imu heading
     pros::delay(10);
     nextSplit("Top wallstake");
 
@@ -80,11 +81,11 @@ void auton::skills() {
     chassis.moveTimed(-60, 0, 500, false);
     arm.moveToPosition(Arm::idle);
     chassis.brake();
-    // Grab ring for alliance stake
-    intake.setMode(Intake::modes::INDEX);
+    // Grab rings
     intake.forwards();
     chassis.turnToPoint(46, 46, 1000, {.direction = lemlib::AngularDirection::CCW_COUNTERCLOCKWISE});
     chassis.moveToPoint(46, 46, 1000, {}, false);
+    intake.setMode(Intake::modes::INDEX);
     pros::delay(300);
     // Go to push goal
     chassis.safeMoveToPoint(46, 29, 600, 1000, {.direction = lemlib::AngularDirection::CW_CLOCKWISE}, {}, false);
@@ -103,27 +104,29 @@ void auton::skills() {
     nextSplit("Corner Mogo");
 
     // Grab 3rd goal
-    robot::safeGrabMogo(51, -8, 2500);
+    robot::safeGrabMogo(49, -15, 2500);
     robot::printPose();
     intake.setMode(Intake::modes::CONTINUOUS);
-    intake.idle();
+    intake.forwards();
     nextSplit("Third Goal");
 
     // Cross field diagonally
-    chassis.turnToPoint(36, 36, 600, {.minSpeed = 0});
-    chassis.moveToPoint(36, 36, 1000, {.maxSpeed = 80, .minSpeed = 0}, false);
+    chassis.turnToPoint(36, 28, 600, {.minSpeed = 0});
+    chassis.moveToPoint(36, 28, 1000, {.maxSpeed = 80, .minSpeed = 0}, false);
     chassis.brake();
     chassis.turnToPoint(0, 0, 1000, {}, false);
     pros::delay(500);
     intake.setMode(Intake::modes::INDEX);
+    intake.forwards();
     chassis.moveToPoint(0, 0, 1000, {.maxSpeed = 70}, false);
     chassis.moveToPoint(-24, -24, 1000, {.maxSpeed = 70}, false);
     intake.setMode(Intake::modes::CONTINUOUS);
-    chassis.moveToPoint(-40, -39, 1000);
+    chassis.moveToPoint(-46, -46, 1000);
     chassis.swingToHeading(180, lemlib::DriveSide::LEFT, 800, {}, false);
     chassis.moveTimed(50, 0, 750, false);
     chassis.turnToHeading(-45, 750, {.minSpeed = 50, .earlyExitRange = 5}, false);
     chassis.swingToHeading(0, lemlib::DriveSide::RIGHT, 400, {}, false);
+    chassis.moveTimed(50, 0, 500);
     // Drop in corner
     chassis.turnToPoint(-70, -70, 750, {.forwards = false});
     chassis.moveTimed(-50, 0, 750, false);
@@ -133,53 +136,24 @@ void auton::skills() {
 
     // Grab 4th goal
     chassis.moveTimed(50, 0, 500, false);
-    robot::safeGrabMogo(-48, -24, 1000);
-    // Bottom wall stake
-    intake.setMode(Intake::modes::INDEX);
-    intake.forwards();
-    chassis.safeMoveToPoint(-24, -48, 1000, {.minSpeed = 0}, false);
-    chassis.moveToPoint(-6, -51, 1000, {.maxSpeed = 70}, false);
-    chassis.swingToHeading(180, lemlib::DriveSide::RIGHT, 500, {}, false);
-    chassis.moveTimed(70, 0, 500, false);
-    chassis.moveTimed(-50, 0, 300);
-    pros::delay(50);
-    arm.moveToPosition(Arm::wall);
-    chassis.brake();
-    robot::scoreWallStake(true, true);
-    chassis.setPose(0, -61, 0); // Odom reset on bottom wallstake
-    nextSplit("Bottom wallstake");
-
-    chassis.moveTimed(-50, 0, 500, false);
-    arm.moveToPosition(Arm::idle);
+    robot::safeGrabMogo(-45, -17, 1000);
+    // Fill goal
     intake.setMode(Intake::modes::CONTINUOUS);
     intake.forwards();
-    chassis.safeMoveToPoint(24, -24, 1000, {}, false);
-    chassis.turnToHeading(-150, 750);
-    chassis.moveToPoint(24, -47, 1000, {}, false);
-    chassis.safeMoveToPoint(43, -55, 1000);
-    chassis.swingToHeading(0, lemlib::DriveSide::LEFT, 600);
-    chassis.swingToHeading(90, lemlib::DriveSide::RIGHT, 600);
-
-    // Place last mogo in corner
-    chassis.turnToPoint(70, -70, 750);
-    chassis.moveTimed(50, 0, 750, false);
-    intake.idle();
-    pros::delay(100);
-    chassis.turnToHeading(-45, 750);
-    chassis.moveTimed(-50, 0, 750);
+    chassis.safeMoveToPoint(-24, -48, 1000);
+    chassis.safeMoveToPoint(0, -60, 1000);
+    chassis.safeMoveToPoint(46, -60, 1000);
+    chassis.safeMoveToPoint(48, -48, 1000);
+    chassis.safeMoveToPoint(24, -48, 1000, false);
+    chassis.safeMoveToPoint(55, -55, 1000, {.forwards = false}, false);
     mogoMech.release(true);
-    nextSplit("Last mogo");
-
-    // Hang
-    chassis.moveTimed(70, 40, 500);
-    intake.forwards();
-    chassis.swingToHeading(135, lemlib::DriveSide::LEFT, 750, {.minSpeed = 100});
-    chassis.turnToHeading(-45, 750, {}, false);
+    chassis.moveTimed(-50, 0, 500);
+    chassis.safeMoveToPoint(24, -24, 1000, false);
+    chassis.turnToPoint(0, 0, 750);
     arm.moveToPosition(Arm::hang);
-    intake.idle();
-    chassis.moveTimed(60, 0, 1000);
+    chassis.moveTimed(127, 0, 500);
+    chassis.moveTimed(70, 0, 1250, false);
     arm.moveToPosition(Arm::idle);
-    pros::delay(500);
+    pros::delay(1500);
     arm.moveToPosition(Arm::hang);
-    nextSplit("Hang");
 }
